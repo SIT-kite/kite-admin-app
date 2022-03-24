b3ebecf3f3df973ddae8eb253cefd5319311edd0
Zhangzqs <i@zhangzqs.cn>
2022-03-24 17:23:48 +0800

    Add: 优化home菜单构造

--- lib/feature/home/page/index.dart
+++ lib/feature/home/page/index.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Widget buildMainMenu(String title, String path) {
+    return Builder(
+      builder: (context) {
+        return ListTile(
+          title: Text(title),
+          onTap: () {
+            Navigator.of(context).pushNamed(path);
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +27,9 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: ListView(
           children: [
-            ListTile(
-              title: const Text('图书馆管理'),
-              onTap: () {
-                Navigator.of(context).pushNamed('/library');
-              },
-            ),
+            buildMainMenu('图书馆预约管理', '/library'),
+            buildMainMenu('反馈', '/feedback'),
+            buildMainMenu('关于', '/about'),
           ],
         ),
       ),
