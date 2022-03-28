8b5b3c2ffb961444ff3e21a5b2c8088bdc05cbc4
sunnysab <i@sunnysab.cn>
2022-03-28 09:04:21 +0800

    Add: 图书馆预约导出.

--- lib/feature/library/appointment/page/index.dart
+++ lib/feature/library/appointment/page/index.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kite_admin/feature/library/appointment/init.dart';
 import 'package:kite_admin/feature/library/appointment/page/result.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pluto_grid/pluto_grid.dart';
 
 import '../entity.dart';
@@ -145,6 +149,23 @@ class LibraryPage extends StatelessWidget {
             onPressed: () {
               app.value = null;
               reloadApplicationList();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.download_outlined),
+            onPressed: () async {
+              final applicationList = app.value;
+              final path = (await getTemporaryDirectory()).path +
+                  '/${selectedDate.value.month}月${selectedDate.value.day}日导出数据.csv';
+
+              final file = File(path);
+              String content = '申请ID,场次,学号,序号,状态\n';
+              for (final e in applicationList!) {
+                content +=
+                    '${e.id},${periodToString(e.period % 10)},${e.user},${e.index},${e.status == 1 ? "已入馆" : "未入馆"}\n';
+              }
+              file.writeAsString(content);
+              OpenFile.open(path, type: 'text/csv');
             },
           ),
         ],
